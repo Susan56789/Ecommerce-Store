38e3f812e4ae6909bbdf7bb8a77eab6530386f4e
Susan Wairimu <mwihaki2susan@gmail.com>
2021-10-22 20:26:15 +0300

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
-import Products from './components/products/Products';
+//import Products from './components/products/Products';
+//import Navbar from './components/Navbar/Navbar';
+
+import {Products, Navbar} from './components';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
 <Products />
     </div>
   );
