b29dd2d87bcaf32b8cd88756698bb692f6187b33
Susan Wairimu <mwihaki2susan@gmail.com>
2021-10-22 19:41:22 +0300

    Added Products

--- src/components/products/Product/Product.jsx
+++ src/components/products/Product/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
-import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
-import { mergeClasses } from '@material-ui/styles';
+import { AddShoppingCart} from '@material-ui/icons';
+
 
 import useStyles from './styles';
 
@@ -9,7 +9,7 @@ const Product = ({product}) => {
     const classes = useStyles();
     return (
         <Card className={classes.root}>
-<CardMedia className={classes.media} image={''} title={product.name}/>
+<CardMedia className={classes.media} image={product.image} title={product.name}/>
        
        <CardContent>
            <div className={classes.cardContent}>
@@ -20,7 +20,7 @@ const Product = ({product}) => {
 {product.price}
 </Typography>
            </div>
-           <Typography variant='h2' color='textSecondary'>
+           <Typography variant='body2' color='textSecondary'>
 {product.description}
            </Typography>
        </CardContent>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,8 +4,8 @@ import Product from './Product/Product';
 
 
 const products = [
-{id:1, name:'shoes', description:'Running Shoes', price:'Ksh. 2000'},
-{id:2, name:'dress', description:'Red Dress', price:'Ksh. 700'}
+{id:1, name:'shoes', description:'Running Shoes', price:'Ksh. 2000', image:'https://media.istockphoto.com/photos/running-shoes-picture-id1249496770?b=1&k=20&m=1249496770&s=170667a&w=0&h=_SUv4odBqZIzcXvdK9rqhPBIenbyBspPFiQOSDRi-RI='},
+{id:2, name:'dress', description:'Red Dress', price:'Ksh. 700', image:'https://media.istockphoto.com/photos/beautiful-young-woman-in-red-dress-picture-id1314032569?b=1&k=20&m=1314032569&s=170667a&w=0&h=VQRDKGoXA3zgBvd-xqNg-ei_lmHsPOCsOVtPXoIvy7Y='}
 ];
 
 const Products = () =>{
